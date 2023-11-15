b226146658655ff3247f7568ad71970fb6830ac9
kumarvishal16 <kumarvishal.16997@gmail.com>
2023-11-15 13:30:34 +0530

    updated project2 data response

--- project2/index.js
+++ project2/index.js
@@ -14,6 +14,7 @@ const packageDetail = {
 let data = [
   { id: 1, name: 'Item 1', ...packageDetail },
   { id: 2, name: 'Item 2', ...packageDetail },
+  { id: 3, name: 'Item 3', ...packageDetail },
 ];
 // Get all items
 app.get('/items', (req, res) => {
